50cdbec6507f5fb7881e402c9ea62b6dbba7a707
Administrator <Michael-glx@163.com>
2023-04-29 23:48:44 +0800

    cross chain transfer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+// Import ABI Code to interact with smart contract
+import cctAbi from "./contracts/CrossChainToken.json";
+
+// The contract address
+const sepoliaContractAddress = "0x98fF74B788aaddf98f3af977960eC50fE0aA2831";
+const goerliContractAddress = "0xF726e94AfA1603a2f9917004F4515E2903b0fede";
 
 function App() {
+  const [account, setAccount] = useState("")
+  const [balance, setBalance] = useState(0)
+
+  // Requests access to the user's Meta Mask Account
+  // https://metamask.io/
+  async function requestAccount() {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  // Fetches account balance
+  async function fetchBalance() {
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        goerliContractAddress,
+        cctAbi,
+        provider
+      );
+      const account = (await provider.getSigner().getAddress()).toString();
+      console.log("Account: ", account);
+      try {
+        const data = (await contract.balanceOf(account)).toString();
+        console.log("data: ", data);
+        setAccount(account);
+        setBalance(data);
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    }
+  }
+
+  // Cross chain transfer of a certain amount of tokens
+  async function transfer(amount) {
+    if (!amount) return;
+
+    console.log("Transfer amount: ", amount);
+
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(goerliContractAddress, cctAbi, signer);
+      const transaction = await contract.bridge(amount, {value: ethers.utils.parseUnits("12345678", "gwei")});
+
+      await transaction.wait();
+      fetchBalance();
+    }
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault()
+    await transfer(event.target.amountInput.value)
+    event.target.amountInput.value = ""
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full max-w-lg container">
+      <div className="shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4">
+        <div className="text-gray-600 font-bold text-lg mb-2">
+          Goerli testnet
+        </div>
+        <div className="w-full border-4 p-2 mb-4 rounded border-gray-400">
+          <div className="text-gray-600 font-bold text-md mb-2">
+            Fetch Account Balance From Smart Contract
+          </div>
+          <div className="flex ">
+            <button 
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={fetchBalance}
+              >Fetch balance</button>
+          </div>
+        </div>
+        <div className="w-full border-4 p-2 mb-4 rounded border-gray-400">
+          <div className="text-gray-600 font-bold text-md mb-2">
+            Cross Chain Transfer
+          </div>
+          <form 
+            className="flex items-center justify-between"
+            onSubmit={event=>handleSubmit(event)}
+          >
+            <input
+             className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+             name="amountInput"
+              />
+              <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Transfer</button>
+          </form>
+        </div>
+        <div className="w-full border-4 p-2 mb-4 rounded border-gray-400 bg-gray-100">
+          <div className="text-gray-600 font-bold text-md mb-2">
+            Account {account} balance
+          </div>
+          <p>
+            {balance}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
